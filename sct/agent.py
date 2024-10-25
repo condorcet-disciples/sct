51be2c9dc0cb72f9da659abf5c967f0c3fd58f3b
edsml-mw2124 <miles.weberman24@imperial.ac.uk>
2024-10-25 17:15:40 +0100

    added classes and other files

---
+++ sct/agent.py
@@ -0,0 +1,9 @@
+class Agent():
+    def __init__(self, preferences, name=None, coef=1):
+        """TODO: add docstrings
+        
+        """
+        self.preferences = preferences
+        self.name = name
+        self.coef = coef
+
